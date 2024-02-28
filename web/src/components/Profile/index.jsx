81b3cc1056c5e7bb97cfa9316e89919f26c95828
TRAMMA\Hp <mavutotram123@gmail.com>
2024-02-28 13:56:53 +0700

    update-avatar

--- web/src/components/Profile/index.jsx
+++ web/src/components/Profile/index.jsx
@@ -1,27 +1,30 @@
-import React, {  useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Button, Form, Image, Modal } from "react-bootstrap";
 import clsx from "clsx";
 import style from "./profile.module.scss";
 import { MdCameraAlt } from "react-icons/md";
 import { CiEdit } from "react-icons/ci";
 import { getUserStorage } from "../../Utils";
-import { putApiWithToken } from "../../API";
+import { getApiWithToken, postApiWithToken, putApiWithToken } from "../../API";
 import Swal from "sweetalert2";
 
 export default function Profile(props) {
   const inputFileReference = useRef(null);
-  const [urlImage, setUrlImage] = useState("");
   const [isUpdate, setIsUpdate] = useState(false);
-  const [user, setUser] = useState(getUserStorage().user);
+  const [isUpdateAvatar, setIsUpdateAvatar] = useState(false);
 
-  const [inputName, setInputName] = useState(user.name);
-  const [inputGender, setInputGender] = useState(user.gender);
-  const [inputDoB, setInputDoB] = useState(user.dateOfBirth);
-  const [inputPW, setInputPW] = useState(user.password);
-
+  const [user, setUser] = useState();
+  const [urlImage, setUrlImage] = useState();
+  const [inputName, setInputName] = useState();
+  const [inputGender, setInputGender] = useState();
+  const [inputDoB, setInputDoB] = useState();
+  const [inputPW, setInputPW] = useState();
+  const [inputAvatar, setInputAvatar] = useState();
 
   const uploadImage = async () => {
     const selectedFile = inputFileReference.current.files[0];
+    console.log(selectedFile);
+    setInputAvatar(selectedFile);
     const url = URL.createObjectURL(selectedFile);
     await setUrlImage(url);
   };
@@ -40,7 +43,7 @@ export default function Profile(props) {
       );
       if (result.status === 200) {
         setUser(result.data);
-        setIsUpdate(!isUpdate)
+        setIsUpdate(!isUpdate);
         Swal.fire({
           icon: "success",
           text: "Successfully updated user information",
@@ -48,6 +51,47 @@ export default function Profile(props) {
       }
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const handleUpdateAvatar = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", inputAvatar);
+      const result = await postApiWithToken(
+        `/users/uploadAvatar/${user._id}`,
+        formData
+      );
+      if (result.status === 200) {
+        setUser(result.data);
+        setIsUpdate(false);
+        setIsUpdateAvatar(false);
+        Swal.fire({
+          icon: "success",
+          text: "Successfully updated avatar information",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (props.show) {
+      fetchData();
+    }
+  }, [props.show]);
+  const fetchData = async () => {
+    const result = await getApiWithToken(`/users/${getUserStorage().user._id}`);
+    if (result.status === 200) {
+      setUser(result.data);
+      setInputName(result.data.name)
+      setInputDoB(result.data.dateOfBirth)
+      setInputGender(result.data.gender)
+      setUrlImage(result.data?.avatar)
+      setInputPW(result.data.password)
+    } else {
+      console.log(result);
     }
   };
 
@@ -62,10 +106,82 @@ export default function Profile(props) {
         closeButton
         className={clsx(style.modalHeader)}
       ></Modal.Header>
-      <Modal.Body className={clsx(style.modalBody)}>
-        <span>
+      {!isUpdateAvatar ? (
+        <Modal.Body className={clsx(style.modalBody)}>
+          <span>
+            <Image
+              className={clsx(style.modalImg)}
+              src={
+                urlImage
+                  ? urlImage
+                  : "https://static.vecteezy.com/system/resources/previews/020/911/740/original/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png"
+              }
+            />
+            <Button
+              // onClick={() => {
+              //   inputFileReference.current.click();
+              // }}
+              // onChange={() => uploadImage()}
+              onClick={() => setIsUpdateAvatar(!isUpdateAvatar)}
+              disabled={isUpdate ? "" : "disabled"}
+            >
+              <MdCameraAlt size={25} />
+            </Button>
+          </span>
+          <Form.Control
+            id="inputText-02"
+            type="text"
+            placeholder="Name"
+            name="name"
+            disabled={isUpdate ? "" : "disabled"}
+            value={inputName}
+            onChange={(e) => setInputName(e.target.value)}
+          />
+          <Form.Control
+            id="inputText-02"
+            type="date"
+            name="dateOfBirth"
+            placeholder="Date of Birth"
+            disabled={isUpdate ? "" : "disabled"}
+            value={inputDoB}
+            onChange={(e) => setInputDoB(e.target.value)}
+          />
+          <Form.Group>
+            <Form.Check
+              inline
+              label="Female"
+              name="gender"
+              type={"radio"}
+              value={"female"}
+              disabled={isUpdate ? "" : "disabled"}
+              defaultChecked={inputGender === "female"}
+              onChange={(e) => setInputGender(e.target.value)}
+            />
+            <Form.Check
+              inline
+              label="Male"
+              name="gender"
+              type={"radio"}
+              value={"male"}
+              disabled={isUpdate ? "" : "disabled"}
+              defaultChecked={inputGender === "male"}
+              onChange={(e) => setInputGender(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Control
+            id="inputText-02"
+            type="password"
+            name="password"
+            placeholder="Password"
+            disabled={isUpdate ? "" : "disabled"}
+            value={inputPW}
+            onChange={(e) => setInputPW(e.target.value)}
+          />
+        </Modal.Body>
+      ) : (
+        <Modal.Body className={clsx(style.modalBody)}>
           <Image
-            className={clsx(style.modalImg)}
+            className={clsx(style.modalImg_2)}
             src={
               urlImage
                 ? urlImage
@@ -73,66 +189,23 @@ export default function Profile(props) {
             }
           />
           <Button
+            className={clsx(style.btnUpload)}
             onClick={() => {
               inputFileReference.current.click();
             }}
             onChange={() => uploadImage()}
-            disabled={isUpdate ? "" : "disabled"}
           >
-            <MdCameraAlt size={25} />
-            <input type="file" hidden ref={inputFileReference} />
+            Upload images from your computer
+            <input
+              type="file"
+              accept="image/jpeg, image/png"
+              hidden
+              ref={inputFileReference}
+            />
           </Button>
-        </span>
-        <Form.Control
-          id="inputText-02"
-          type="text"
-          placeholder="Name"
-          name="name"
-          disabled={isUpdate ? "" : "disabled"}
-          value={inputName}
-          onChange={(e) => setInputName(e.target.value)}
-        />
-        <Form.Control
-          id="inputText-02"
-          type="date"
-          name="dateOfBirth"
-          placeholder="Date of Birth"
-          disabled={isUpdate ? "" : "disabled"}
-          value={inputDoB}
-          onChange={(e) => setInputDoB(e.target.value)}
-        />
-        <Form.Group>
-          <Form.Check
-            inline
-            label="Female"
-            name="gender"
-            type={"radio"}
-            value={"female"}
-            disabled={isUpdate ? "" : "disabled"}
-            defaultChecked={inputGender==='female'}
-            onChange={(e) => setInputGender(e.target.value)}
-          />
-          <Form.Check
-            inline
-            label="Male"
-            name="gender"
-            type={"radio"}
-            value={"male"}
-            disabled={isUpdate ? "" : "disabled"}
-            defaultChecked={inputGender==='male'}
-            onChange={(e) => setInputGender(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Control
-          id="inputText-02"
-          type="password"
-          name="password"
-          placeholder="Password"
-          disabled={isUpdate ? "" : "disabled"}
-          value={inputPW}
-          onChange={(e) => setInputPW(e.target.value)}
-        />
-      </Modal.Body>
+        </Modal.Body>
+      )}
+
       <Modal.Footer className={clsx(style.modalBody)}>
         {!isUpdate ? (
           <Button
@@ -144,10 +217,18 @@ export default function Profile(props) {
           </Button>
         ) : (
           <div className={clsx(style.btnGroup)}>
-            <Button onClick={() => handleUpdateInfo()}>Update</Button>
+            {isUpdateAvatar ? (
+              <Button onClick={() => handleUpdateAvatar()}>
+                Update Avatar
+              </Button>
+            ) : (
+              <Button onClick={() => handleUpdateInfo()}>Update</Button>
+            )}
             <Button
               onClick={() => {
                 setIsUpdate(!isUpdate);
+                setIsUpdateAvatar(false);
+                setUrlImage(user.avatar);
               }}
             >
               Cancel
